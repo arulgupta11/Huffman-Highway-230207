4550ba04ca038da8064485fa237fbee560751d52
arulgupta11 <arulguptaun@gmail.com>
2024-06-20 12:20:52 +0530

    Update Q2.cpp

--- Assignment3/Q2.cpp
+++ Assignment3/Q2.cpp
@@ -1,2 +1,2 @@
-In the third line, the address will be printed because arr is actually the pointer to the first element of the integer array.
-In the fourth line, "abcde" will be printed as the ch is a character string and program reads its charadcters until it uncounters the null character
+//In the third line, the address will be printed because arr is actually the pointer to the first element of the integer array.
+//In the fourth line, "abcde" will be printed as the ch is a character string and program reads its charadcters until it uncounters the null character
